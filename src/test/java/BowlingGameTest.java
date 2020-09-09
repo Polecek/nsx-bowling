aebac30598e503b51d24a0ee650564644419e28b
Hrdina, Pavel <phrdina@netsuite.com>
2020-09-09 10:32:05 +0200

    Extracting game initialization in the test

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -4,14 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BowlingGameTest {
 
+    private final BowlingGame game = new BowlingGame();
+
     @Test
     void emptyGame() {
-        assertEquals(0, new BowlingGame().getScore());
+        assertEquals(0, game.getScore());
     }
 
     @Test
     void firstThrow() {
-        BowlingGame game = new BowlingGame();
         game.registerBallThrown(1);
         assertEquals(1, game.getScore());
     }
